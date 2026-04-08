ed03b37d081843e23107e9e50e25724e9aed2dd4
Trikoper <96494412+Trikoper@users.noreply.github.com>
2026-04-08 11:00:20 +0300

    Added the functionality to show the note and ability to delete it

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # toDoList
 Goal: Simple to do list for browser, where we can add new notes, delete or edit existing ones with the feature to automaticly all locally
 
-Current state: We have an input zone
+Current state: 
+1)We have an input zone
+2)By entering an input we get the note displayed
+3)Using X button we can delete notes
--- script.js
+++ script.js
@@ -2,3 +2,28 @@ const form = document.querySelector('form');
 const todoInput = document.querySelector('#todo-input');
 const todoContent = document.querySelector('#todo-content');
 
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('is-flex');
+
+    const todoText = document.createElement('h3');
+    todoText.textContent = todoInput.value;
+
+    const todoDelete = document.createElement('button');
+    todoDelete.textContent = 'X';
+    todoDelete.setAttribute('id', 'delete');
+
+    todoDiv.appendChild(todoDelete);
+    todoDiv.appendChild(todoText);
+
+    todoContent.appendChild(todoDiv);
+    form.reset();
+})
+
+
+todoContent.addEventListener("click", e => {
+    const target = e.target;
+    if(target.getAttribute('id') == 'delete') todoContent.removeChild(e.target.parentNode);
+})
